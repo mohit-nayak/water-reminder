51f720af8186aa4ff6723283a7d6744e8fa5d148
Mohit Nayak <mohitn21@gmail.com>
2022-08-27 21:00:46 +0530

    added now config

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
 ) // for parsing application/x-www-form-urlencoded
 
 //This is the route the API will call
-app.post("/new-message", function(req, res) {
+app.post("/", function(req, res) {
     const { message } = req.body
 
     //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
