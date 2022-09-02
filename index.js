344d92a105b5c3bb7d97881e5e3901203dde1ecf
Mohit Nayak <mohitn21@gmail.com>
2022-09-03 02:27:20 +0530

    testing socket recieve msg-2

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bot = new TelegramBot(TELEGRAM_TOKEN, "5054842976");
 async function main() {
     //The first argument is the matching text for the callback to be executed
     //The second argument is the callback function
+    await bot.sendMessage("Random text");
     // let inputMsg;
     await bot.onMessage("Hi", async () => {
         // const responseMsg = responseFor("Hi");
