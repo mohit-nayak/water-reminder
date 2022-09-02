d6056e7f2fdf6b1f78d98f09a80eda246349343c
Mohit Nayak <mohitn21@gmail.com>
2022-09-03 02:28:32 +0530

    testing socket recieve msg-23

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { TelegramBot } = require("telegram-bot-nodejs");
-const { responseFor } = require("./utils");
 
 const TELEGRAM_TOKEN = '5743867232:AAEqMVYKx3WHXfrKLsrtEoid_sY9mEwcg78';
 
@@ -8,11 +7,8 @@ const bot = new TelegramBot(TELEGRAM_TOKEN, "5054842976");
 async function main() {
     //The first argument is the matching text for the callback to be executed
     //The second argument is the callback function
-    await bot.sendMessage("Random text");
-    // let inputMsg;
     await bot.onMessage("Hi", async () => {
-        // const responseMsg = responseFor("Hi");
-        await bot.sendMessage("Random text");
+        await bot.sendMessage("Hello");
     });
 }
 
