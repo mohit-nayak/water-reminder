65941528259998ef9da7116ffceee57cd35f0b7e
Mohit Nayak <mohitn21@gmail.com>
2022-09-03 02:33:20 +0530

    testing socket recieve msg-3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const TELEGRAM_TOKEN = '5743867232:AAEqMVYKx3WHXfrKLsrtEoid_sY9mEwcg78';
 const bot = new TelegramBot(TELEGRAM_TOKEN, "5054842976");
 
 async function sendMessage() {
-    const response = await bot.sendMessage("Hello world");
+    const response = await bot.sendMessage("Water reminder. Stay hydrated!");
     console.log(response);
 }
 
