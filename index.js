b6658b552f88bb93b87b6cd73c465bf200a460e8
Mohit Nayak <mohitn21@gmail.com>
2022-09-03 03:22:46 +0530

    testing socket recieve msg1

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const TOKEN = '5743867232:AAEqMVYKx3WHXfrKLsrtEoid_sY9mEwcg78';
-const url = 'https://water-reminder-khaki.vercel.app';
-const port = 3000;
+// const url = 'https://water-reminder-khaki.vercel.app';
+// const port = 3000;
 
 const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
+// const express = require('express');
 
 // No need to pass any parameters as we will handle the updates with Express
-const bot = new TelegramBot(TOKEN);
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 // This informs the Telegram servers of the new webhook.
-bot.setWebHook(`/`);
+// bot.setWebHook(`/`);
 
-const app = express();
+// const app = express();
 
 // parse the updates to JSON
-app.use(express.json());
+// app.use(express.json());
 
 // We are receiving updates at the route below!
+/*
 app.post(`/`, (req, res) => {
     console.log("requested")
     bot.processUpdate(req.body);
@@ -31,4 +32,24 @@ app.listen(port, () => {
 // Just to ping!
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, 'I am alive!');
-});
+});*/
+
+let interval;
+let defaultTime = 15;
+
+bot.on('message', (msg) => {
+    const chatID = msg.from.id;
+    const message = msg.text.toLowerCase();
+
+    if (message.includes("start")) {
+        const time = ((Number(message.split(" ")[1])) || defaultTime) * 60000;
+        console.log("starting for time ", time);
+        interval = setInterval(() => {
+            bot.sendMessage(chatID, "Drink water!");
+        }, time);
+    }
+
+    if (message === "stop") {
+        clearInterval(interval);
+    }
+})
